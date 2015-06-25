63177cf6cb345327aeb75c383b653a4da301a66e
amunozgarcia <elsocorrista@gmail.com>
2015-06-25 18:54:31 +0200

    Actualización

--- src/Jamba/Ws/Middleware/WsMiddleware.php
+++ src/Jamba/Ws/Middleware/WsMiddleware.php
@@ -1,9 +1,9 @@
 <?php namespace Jamba\Ws\Middleware;
 
 use Closure;
-use Illuminate\Contracts\Routing\Middleware;
+use Illuminate\Support\Facades\Request;
 
-class WsMiddleware implements Middleware
+class WsMiddleware
 {
 
     /**
@@ -15,7 +15,25 @@ class WsMiddleware implements Middleware
      */
     public function handle($request, Closure $next)
     {
-        dd("Entro en el WsMiddleware");
+        //saco el listado de ip's con acceso
+        $secure = config('ws.secure');
+        //compruebo si la ip de la consulta esta en la lista de acceso
+        if(!in_array(Request::getClientIp(), $secure))
+        {
+            //si no estoy en la lista de ip compruebo si es ejecutada la consulta con el dominio
+            if (!empty($_SERVER['HTTP_REFERER']))
+            {
+                //si el dominio viene en la lista de seguros dejo pasar la consulta
+                if(in_array(getdomain($_SERVER['HTTP_REFERER']), $secure))
+                {
+                    return $next($request);
+                }
+            }
+
+            //pinto error 500 (acceso denegado)
+            return response("Acceso denegado", 500);
+        }
+
         return $next($request);
     }
 }
--- src/helpers.php
+++ src/helpers.php
@@ -1,13 +1,82 @@
 <?php
 
-if (!function_exists('ws_fito')) {
+if (!function_exists('getdomain')) {
+
+    /**
+     * @param $url
+     * @return string
+     */
+    function getdomain($url)
+    {
+        $components = parse_url($url);
+        preg_match('/\.([^\/]+)/', $components['host'], $domain);
+        return strtolower($domain[1]);
+    }
+}
+
+if (!function_exists('ws')) {
+
+    /**
+     * Conexión y consulta al webservice establecido en la configuración
+     * @param $cond
+     * @param array $param
+     * @return mixed
+     */
+    function ws($cond, $param)
+    {
+        return app()->make('Jamba\Ws\Facade\Ws')->get($cond, $param);
+    }
+}
+
+
+if (!function_exists('ws_middleware_filter_ip')) {
+
+    /**
+     * Conexión y consulta al webservice establecido en la configuración
+     * @param $cond
+     * @param array $param
+     * @return mixed
+     */
+    function ws_middleware_filter_ip(\App\Http\Controllers\Controller $app)
+    {
+        return app()->make('Jamba\Ws\Facade\Ws')->filtroIpMiddleware($app);
+    }
+}
+
+if (!function_exists('ws_get_error')) {
 
     /**
      * Devuelve la descripción del ultimo error
      * @return boolean mixed
      */
-    function ws_fito()
+    function ws_get_error()
     {
-        return "Entra en fito";
+        return app()->make('Jamba\Ws\Facade\Ws')->getError();
+    }
+}
+
+if (!function_exists('ws_is_error')) {
+
+    /**
+     * Devuelve si la ultima consulta contiene un error
+     * @return boolean mixed
+     */
+    function ws_is_error()
+    {
+        return (bool) app()->make('Jamba\Ws\Facade\Ws')->isError();
+    }
+}
+
+if (!function_exists('ws_filter')) {
+
+    /**
+     * Filtro WS
+     * @param $filter
+     * @param $data
+     * @return mixed
+     */
+    function ws_filter($filter, $data)
+    {
+        return app()->make('Jamba\Ws\Facade\Ws')->filter($filter, $data);
     }
 }
